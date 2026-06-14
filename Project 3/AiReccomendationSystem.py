c245fa7900c0960b487a1e654dda7547d756faa0
Dinuli973 <dinulisamaraweera@gmail.com>
2026-06-14 10:49:16 +0530

    Creating a file for ai recommendation. Ask user preferences and store recommendations.

---
+++ Project 3/AiReccomendationSystem.py
@@ -0,0 +1,12 @@
+print("Simple Movie Recommendation System")
+print("-------------------------------")
+
+genre=input("Enter a genre(Action,Comedy,Sci-Fi,Horror):)")
+
+movies={
+    "Action":["John Wick","Mission Impossible","Fast and Furious"],
+    "Comedy":["Mr.Bean","Home Alone","Friends","The Mask"],
+    "Sci-Fi":["Interstellar","The Matrix","Avatar"],
+    "Horror":["Conjuring","Annabelle","Scream","Insidious"]
+}
+
